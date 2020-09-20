69f4e2519065bc4822547a52f71fed6b4fd170f8
wisit97011 <69495754+wisit97011@users.noreply.github.com>
2020-09-20 20:00:47 +0700

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int main()
+{
+    char number[10];
+    int check = 0;
+    int i = 0;
+
+    printf("Enter a number: ");
+    scanf("%s", number);
+    while (number[i++] != '\0')
+    {
+        if (number[i] == '.')
+        {
+            check = 1;
+            break;
+        }
+    }
+
+    if (check)
+        printf("Input Number is a Floating Number");
+    else
+        printf("Input Number is a integer Number");
+
+    return 0;
+}
